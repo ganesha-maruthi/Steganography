4473dac5056492e1774d43cb2baef5975ff60d2d
Ganesha Maruthi <ultimatechanti@gmail.com>
2022-04-28 23:04:05 +0530

    Medium handles Audio

--- app/src/main/java/Steganography/Medium.java
+++ app/src/main/java/Steganography/Medium.java
@@ -1,8 +1,8 @@
-// commented code for handling Audio; must uncomment after merging code
-
 package Steganography;
 
-import java.io.IOException;
+import java.io.*;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 enum MediumType {
 	audio,
@@ -28,11 +28,10 @@ public class Medium {
 		this.medium_type = medium_type;
     }
 
-	public int lsb(int bits) throws IOException {
+	public int lsb(int bits) throws IOException, UnsupportedAudioFileException {
 		if(this.medium_type == MediumType.audio) {
-			/* Audio au = new Audio(basefilepath, inputfilepath, outputfilepath);
-			return au.lsb(1); */
-			return 1;
+			Audio au = new Audio(basefilepath, inputfilepath, outputfilepath);
+			return au.lsb(1, username, password);
 		}
 		else {
 			Image im = new Image(basefilepath, inputfilepath, outputfilepath);
@@ -40,10 +39,10 @@ public class Medium {
 		}
 	}
 
-	public void read(int bits) throws IOException {
+	public void read(int bits) throws IOException, UnsupportedAudioFileException {
 		if(this.medium_type == MediumType.audio) {
-			/* Audio au = new Audio(basefilepath, outputfilepath);
-			au.read(); */
+			Audio au = new Audio(basefilepath, outputfilepath);
+			au.read(1, username);
 		}
 		else {
 			Image im = new Image(basefilepath, outputfilepath);
